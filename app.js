f7f64ffc8b52c23d3204f7d800275ea8dd676043
Christopher Danan <chrisddan@gmail.com>
2015-05-03 20:04:00 -0700

    Initial commit: -Game can catch hits and misses.

--- app.js
+++ app.js
@@ -5,7 +5,31 @@ var main = function(){
 
 	//Reference: http://stackoverflow.com/questions/14051821/get-html-table-cells-values-in-a-rows-by-clicking-on-it
 	$("#grid").click(function(cell){
+		console.log("Clicked: " + $(cell.target).attr("id"));
+		//Show what was clicked.
 		$("#clickResult p").text("You clicked: " + ($(cell.target).attr("id")))
+
+		//Get the list of classes for clicked item.
+		var classList = $(cell.target).attr("class").split(" ");
+		var isHit = 0;
+
+		//Go through list of classes to see if the square contained a ship.
+		classList.forEach(function(element){
+			if(element === "ship"){
+				console.log("You hit a ship");
+				isHit = 1;
+				$(cell.target).addClass("hit");  //If it held a ship, mark it as "hit".
+			}
+		});
+
+		//If cell did not contain ship, then mark it as miss.
+		if(isHit === 0){
+			$(cell.target).addClass("miss");
+		}
+
+		//Make cell unclickable.
+		$(cell.target).click(false);
+		
 	});
 };
 
